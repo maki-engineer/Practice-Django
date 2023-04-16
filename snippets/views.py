792d2d4e49005702511515e16748efbb6a8d14c9
maki-engineer <ryohei-81@outlook.jp>
2023-04-16 11:49:11 +0900

    add: snippeetの詳細画面を表示する機能を実装

--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from snippets.models import Snippet
 
@@ -14,4 +14,6 @@ def snippet_edit(request, snippet_id):
   return HttpResponse("スニペットの編集")
 
 def snippet_detail(request, snippet_id):
-  return HttpResponse("スニペットの詳細閲覧")
+  snippet = get_object_or_404(Snippet, pk=snippet_id)
+
+  return render(request, "snippets/snippet_detail.html", {"snippet": snippet})
